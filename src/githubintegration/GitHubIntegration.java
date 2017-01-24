7ed7b78530ecf6b213f1b5967a9f9737593d08aa
Owner <Owner@DESKTOP-4LFIRB4>
2017-01-23 20:44:45 -0500

    Initial upload

---
+++ src/githubintegration/GitHubIntegration.java
@@ -0,0 +1,17 @@
+
+package githubintegration;
+
+/**
+ *
+ * @author Owner
+ */
+public class GitHubIntegration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Wahoo - I have created a respository in GitHub");
+    }
+    
+}
